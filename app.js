7dfbaf88ecfc20d686d2c49ea0a46b7ce308c7a0
rodrigopluz <rodrigopluz@gmail.com>
2020-03-02 23:15:35 -0300

    atualizando alteraçoes de comentarios

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ var app = new Vue({
         this.getAllTasks();
     },
     methods: {
+        // read - lista todos os registros
         getAllTasks: function () {
             axios.get("http://localhost/api-vuejs/api.php?action=read")
                 .then(function (res) {
-                    console.log(res);
                     if (res.data.error) {
                         app.errorMessage = res.data.message;
                     } else {
@@ -26,11 +26,11 @@ var app = new Vue({
                     }
                 });
         },
+        // cread - cria um novo registro
         saveTasks: function () {
             var formData = app.toFormData(app.newTasks);
             axios.post("http://localhost/api-vuejs/api.php?action=create", formData)
                 .then(function (res) {
-                    console.log(res);
                     app.newTasks = { user: "", task: "", date: "" };
 
                     if (res.data.error) {
@@ -41,27 +41,14 @@ var app = new Vue({
                     }
                 });
         },
+        // update - atualiza um registro selecionado
         updateTasks: function () {
             var formData = app.toFormData(app.clickedTask);
 			axios.post("http://localhost/api-vuejs/api.php?action=update", formData)
-				.then(function(res){
-					console.log(res);
+				.then(function (res) {
 					app.clickedTask = {};
-					if (res.data.error) {
-						app.errorMessage = res.data.message;
-					}else{
-						app.successMessage = res.data.message;
-						app.getAllTasks();
-					}
-				});
-        },
-        deleteTasks: function () {
-            var formData = app.toFormData(app.clickedTask);
-			axios.post("http://localhost/api-vuejs/api.php?action=delete", formData)
-				.then(function(res) {
-					console.log(res);
-					app.clickedTask = {};
-					if (res.data.error) {
+                    
+                    if (res.data.error) {
 						app.errorMessage = res.data.message;
 					} else {
 						app.successMessage = res.data.message;
@@ -69,9 +56,26 @@ var app = new Vue({
 					}
 				});
         },
+        // delete - deleta um registro selecionado
+        deleteTasks: function () {
+            var formData = app.toFormData(app.clickedTask);
+			axios.post("http://localhost/api-vuejs/api.php?action=delete", formData)
+				.then(function (res) {
+					app.clickedTask = {};
+                    
+                    if (res.data.error) {
+						app.errorMessage = res.data.message;
+					} else {
+						app.successMessage = res.data.message;
+						app.getAllTasks();
+					}
+				});
+        },
+        // selectTask - faz a verificação se o usuario clicou no edit ou no delete
         selectTask(task) {
             app.clickedTask = task;
         },
+        // toFormData - função que verifica os parametros passados.
         toFormData: function (obj) {
             var form_data = new FormData();
             for (var key in obj) {
@@ -80,6 +84,7 @@ var app = new Vue({
 
             return form_data;
         },
+        // clearMessage - mostra as mensagem de erro ou de sucesso
         clearMessage: function () {
             app.errorMessage = "";
             app.successMessage = "";
